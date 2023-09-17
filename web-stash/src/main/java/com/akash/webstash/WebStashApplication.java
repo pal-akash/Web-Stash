9b09aba23dbe125c5fc5262b0cb66351b0e76d70
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-17 22:37:22 +0530

    [init] initialize project

---
+++ web-stash/src/main/java/com/akash/webstash/WebStashApplication.java
@@ -0,0 +1,13 @@
+package com.akash.webstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStashApplication.class, args);
+	}
+
+}
---
+++ web-stash/src/test/java/com/akash/webstash/WebStashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.webstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
