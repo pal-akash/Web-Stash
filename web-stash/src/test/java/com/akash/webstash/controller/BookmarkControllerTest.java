265394a2bec36f3b0e247881faa1624981cb1d7f
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-19 02:03:56 +0530

    integration test case with postgres docker container

--- web-stash/src/test/java/com/akash/webstash/controller/BookmarkControllerTest.java
+++ web-stash/src/test/java/com/akash/webstash/controller/BookmarkControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
@@ -20,6 +21,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///bookmarks"
+})
 class BookmarkControllerTest {
 
     @Autowired
